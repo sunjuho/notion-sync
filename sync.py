145b7e5d868757d3fbe2a817c71dc3a9a1ef545e
sunjuho <sun_juho@naver.com>
2022-04-19 17:54:20 +0900

    task object 생성 모듈화

--- sync.py
+++ sync.py
@@ -2,6 +2,30 @@ import notion
 import task
 
 
+# notion page로부터 task에 넣을 형태로 변환
+def get_task_from_notion(page):
+    insert_task = {}
+
+    title = page['properties']['Todo']['title'][0]['text']['content']
+    if title:
+        insert_task["title"] = title
+        print("타이틀 : " + title)
+
+    if page['properties']['완료']['checkbox']:
+        status = "completed"
+    else:
+        status = "needsAction"
+    insert_task["status"] = status
+    print("완료 : " + status)
+
+    if page['properties']['일자']['date']:
+        due = page['properties']['일자']['date']['start'] + "T00:00:00.000Z"
+        insert_task["due"] = due
+        print("일자 : " + due)
+
+    return insert_task
+
+
 # notion에서 읽어서 google task id가 없는, google task 동기화가 안 된 것 찾아서
 # task insert
 # update notion google task id
@@ -13,25 +37,7 @@ def create_task_from_notion(notion_account, task_account):
             print(item)
             page_id = item['id']
 
-            insert_task = {}
-
-            title = item['properties']['Todo']['title'][0]['text']['content']
-            if title:
-                insert_task["title"] = title
-                print("타이틀 : " + title)
-
-            if item['properties']['완료']['checkbox']:
-                status = "completed"
-            else:
-                status = "needsAction"
-            insert_task["status"] = status
-            print("완료 : " + status)
-
-            if item['properties']['일자']['date']:
-                due = item['properties']['일자']['date']['start'] + "T00:00:00.000Z"
-                insert_task["due"] = due
-                print("일자 : " + due)
-
+            insert_task = get_task_from_notion(item)
             """
             insert_task = {
                 "title": title,
